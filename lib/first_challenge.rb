366d8de9ea4fd3d587c625c55bdbcf0af5fd94f1
Mary Kanu <maryikanu@gmail.com>
2019-09-30 09:56:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,7 @@
 def first_challenge do
    #it "sets a variable, epic_tragedy, equal to a hash containing keys of family names and values of empty hashes"
    
-    epic_tragedy = {
+  epic_tragedy = {
   :montague => {},
   :capulet => {}
 }
