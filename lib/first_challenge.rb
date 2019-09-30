fd8c2f3ea51755dc877349bbcdea7faa3cfbe400
Mary Kanu <maryikanu@gmail.com>
2019-09-30 09:55:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,13 +1,15 @@
 def first_challenge do
    #it "sets a variable, epic_tragedy, equal to a hash containing keys of family names and values of empty hashes"
    
+    epic_tragedy = {
+  :montague => {},
+  :capulet => {}
+}
+   
     expect(first_challenge.keys).to eq([:montague, :capulet])
     
     expect(first_challenge.values).to eq([{}, {}])
   
-  epic_tragedy = {
-  :montague => {},
-  :capulet => {}
-}
+ 
   end
    
